01e18e88d408dbbef2658132f5cc7df1db0a0f00
pilgrim1807 <51112693+pilgrim1807@users.noreply.github.com>
2024-10-15 13:37:27 +0300

    Ответ на задание #1

---
+++ data-types.js
@@ -0,0 +1,28 @@
+const string = 'строка';
+console.log('string', string, typeof string);
+
+const time = 57;
+console.log('time', time, typeof time);
+
+const bigNumber = 9997000254999999;
+console.log('bigNumber', bigNumber, typeof bigNumber);
+
+const isYes = true; // Логическое значение "истина"
+console.log('isYes', isYes, typeof isYes);
+
+const age = null; 
+console.log('age', age, typeof age);
+
+const uncertainty = undefined;
+console.log('uncertainty', uncertainty, typeof uncertainty);
+
+const person = {
+    gender: "female",
+    heigh: 172,
+    weight: 54,
+    dateOfBirth: 9022000
+}
+console.log('person', person, typeof person);
+
+const symbol = unique("symbol");
+console.log('symbol', symbol, typeof symbol);
